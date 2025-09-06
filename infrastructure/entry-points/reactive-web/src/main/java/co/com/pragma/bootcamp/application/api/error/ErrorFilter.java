448fc3240b6e63121fe817b611f7dedf3c32336c
dani09barreto <danielf.barreto@javeriana.edu.co>
2025-09-05 20:21:59 -0500

    fix 403 ErrorFilter

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/pragma/bootcamp/application/api/error/ErrorFilter.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/pragma/bootcamp/application/api/error/ErrorFilter.java
@@ -6,6 +6,7 @@ import co.com.pragma.bootcamp.application.usecase.registerapplication.error.User
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.security.authorization.AuthorizationDeniedException;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.HandlerFilterFunction;
 import org.springframework.web.reactive.function.server.HandlerFunction;
@@ -29,7 +30,7 @@ public class ErrorFilter implements HandlerFilterFunction<ServerResponse, Server
                         ex -> buildErrorResponse(HttpStatus.NOT_FOUND, ex.getMessage(), request))
                 .onErrorResume(InvalidUserDataException.class,
                         ex -> buildErrorResponse(HttpStatus.BAD_REQUEST, ex.getMessage(), request))
-                .onErrorResume(AccessDeniedException.class,
+                .onErrorResume(AuthorizationDeniedException.class,
                         ex -> buildErrorResponse(HttpStatus.FORBIDDEN, ex.getMessage(), request))
                 .onErrorResume(Exception.class,
                         ex -> buildErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage(), request));
